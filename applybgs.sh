7b5c6c1522f1722f753a8da52fae304917d255ea
SysWhiteDev <syswhite.commercial@gmail.com>
2023-10-30 21:53:37 +0100

    Added no network failsafe

--- applybgs.sh
+++ applybgs.sh
@@ -3,8 +3,7 @@
 # update background list
 sudo rm -rf /usr/share/backgrounds/hidebackgrounds
 cd /usr/share/backgrounds
-sudo git clone https://github.com/eyehide/hidebackgrounds
-cd ./hidebackgrounds
+sudo git clone https://github.com/eyehide/hidebackgrounds && cd ./hidebackgrounds || { echo "Git clone failed! Exiting..."; exit 1; }
 
 # remove current bgs
 sudo rm -rf /usr/share/backgrounds/gnome/*
@@ -12,4 +11,4 @@ sudo rm -rf /usr/share/gnome-background-properties/*
 
 # import all bgs to current install
 sudo cp ./gnome/* /usr/share/backgrounds/gnome/
-sudo cp ./gnome-background-properties/* /usr/share/gnome-background-properties 
+sudo cp ./gnome-background-properties/* /usr/share/gnome-background-properties
