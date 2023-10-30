e2f5f63d38732b22a1a64ac849d22e81e4e013b5
SysWhiteDev <syswhite.commercial@gmail.com>
2023-10-30 22:14:13 +0100

    Removed needed sudo priviledges

--- applybgs.sh
+++ applybgs.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 # update background list
-sudo rm -rf /usr/share/backgrounds/hidebackgrounds
+rm -rf /usr/share/backgrounds/hidebackgrounds
 cd /usr/share/backgrounds
-sudo git clone https://github.com/eyehide/hidebackgrounds && cd ./hidebackgrounds || { echo "Git clone failed! Exiting..."; exit 1; }
+git clone https://github.com/eyehide/hidebackgrounds && cd ./hidebackgrounds || { echo "Git clone failed! Exiting..."; exit; }
 
 # remove current bgs
-sudo rm -rf /usr/share/backgrounds/gnome/*
-sudo rm -rf /usr/share/gnome-background-properties/*
+rm -rf /usr/share/backgrounds/gnome/*
+rm -rf /usr/share/gnome-background-properties/*
 
 # import all bgs to current install
-sudo cp ./gnome/* /usr/share/backgrounds/gnome/
-sudo cp ./gnome-background-properties/* /usr/share/gnome-background-properties
+cp ./gnome/* /usr/share/backgrounds/gnome/
+cp ./gnome-background-properties/* /usr/share/gnome-background-properties
