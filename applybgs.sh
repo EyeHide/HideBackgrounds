49dbfb4c13380084be07cb1759724bcfd7735797
SysWhiteDev <syswhite.commercial@gmail.com>
2023-10-30 21:21:14 +0100

    Minor changes

--- applybgs.sh
+++ applybgs.sh
@@ -7,9 +7,9 @@ sudo git clone https://github.com/eyehide/hidebackgrounds
 cd ./hidebackgrounds
 
 # remove current bgs
-sudo rm -rf /usr/share/gnome-background-properties/*
 sudo rm -rf /usr/share/backgrounds/gnome/*
+sudo rm -rf /usr/share/gnome-background-properties/*
 
 # import all bgs to current install
-sudo cp ./gnome-background-properties/* /usr/share/gnome-background-properties
-sudo cp ./gnome/* /usr/share/backgrounds/gnome/ 
+sudo cp ./gnome/* /usr/share/backgrounds/gnome/
+sudo cp ./gnome-background-properties/* /usr/share/gnome-background-properties 
