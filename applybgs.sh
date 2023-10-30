a81c63b9561093b009034328b4095a20c4a5de02
SysWhiteDev <syswhite.commercial@gmail.com>
2023-10-31 00:32:48 +0100

    Added bg refresher

--- applybgs.sh
+++ applybgs.sh
@@ -1,14 +1,23 @@
 #!/bin/bash
 
 # update background list
-rm -rf /usr/share/backgrounds/hidebackgrounds
+sudo rm -rf /usr/share/backgrounds/hidebackgrounds
 cd /usr/share/backgrounds
-git clone https://github.com/eyehide/hidebackgrounds && cd ./hidebackgrounds || { echo "Git clone failed! Exiting..."; exit; }
+sudo git clone https://github.com/eyehide/hidebackgrounds && cd ./hidebackgrounds || { echo "Git clone failed! Exiting..."; exit; }
 
 # remove current bgs
-rm -rf /usr/share/backgrounds/gnome/*
-rm -rf /usr/share/gnome-background-properties/*
+sudo rm -rf /usr/share/backgrounds/gnome/*
+sudo rm -rf /usr/share/gnome-background-properties/*
 
 # import all bgs to current install
-cp ./gnome/* /usr/share/backgrounds/gnome/
-cp ./gnome-background-properties/* /usr/share/gnome-background-properties
+sudo cp ./gnome/* /usr/share/backgrounds/gnome/
+sudo cp ./gnome-background-properties/* /usr/share/gnome-background-properties
+
+# refresh background cfgs
+if [ "$(gsettings get org.gnome.desktop.interface color-scheme)" == "'prefer-dark'" ]; then
+    gsettings set org.gnome.desktop.interface color-scheme "'prefer-light'"
+    gsettings set org.gnome.desktop.interface color-scheme "'prefer-dark'"
+else
+    gsettings set org.gnome.desktop.interface color-scheme "'prefer-dark'"
+    gsettings set org.gnome.desktop.interface color-scheme "'prefer-light'"
+fi
